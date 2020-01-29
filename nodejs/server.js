52be8b3107646ee96c322c413a68d3b3d080ec27
Umyar <umiarka@gmail.com>
2020-01-29 18:45:53 +0300

    local email sending done;

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-// const cors = require('cors');
 const parser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
+
+const transport = nodemailer.createTransport({
+  service: 'Yandex',
+  auth: {
+    user: 'hello@topux.dev',
+    pass: 'QLziMX43A',
+  },
+});
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://topux.dev');
@@ -20,19 +28,27 @@ app.use((req, res, next) => {
 });
 
 app.post('/user-request', function(req, res) {
-  // console.log(req);
-  // let request = req.body;
-
   const { name, email, phone, comment } = req.body;
 
   if (name && (email || phone)) {
-    console.log('name: ', name, 'email: ', email, 'phone: ', phone, 'comment: ', comment);
+    transport.sendMail({
+      from: 'hello@topux.dev',
+      to: 'umiarka@gmail.com',
+      subject: 'Заявка с topux.dev',
+      text: `Имя: ${name}, Email: ${email}, Телефон: ${phone}, Комментарий: ${comment}`,
+      html:
+        `<!DOCTYPE html><html lang="ru"><head> <meta charset="UTF-8"> <title>Заявка</title>` +
+        `</head><body> <h1 style="font-size: 20px">Заявка с topux.dev</h1>` +
+        `<p><b>Имя:</b> ${name}</p><p><b>Email:</b> ${email}</p>` +
+        `<p><b>Телефон:</b> <a href="tel:${phone}">${phone}</a></p><p><b>Комментарий:</b> ${comment}</p></body></html>`,
+    });
+
     res.json({ status: 'ok', message: `Заявка отправлена успешно! Спасибо, ${name}!` });
   } else {
     res.json({
       status: 'fail',
       message:
-        'Извините, но что-то пошло не так. Пожалуйста, напишите об этом нам на topux.dev@gmail.com',
+        'Извините, но что-то пошло не так. Пожалуйста, напишите об этом нам на hello@topux.dev',
     });
   }
 });
@@ -42,4 +58,4 @@ app.get('/test', function(req, res) {
   res.send('Я люблю тебя ❤️');
 });
 
-app.listen(9000, () => console.log('Hello from backend!'));
+app.listen(9000, () => console.log('Backend started on 9000 port!'));
